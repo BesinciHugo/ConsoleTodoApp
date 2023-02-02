83234fb89b2a87971768a2d1f976fcd7f5690999
Alperen KÖSE <kose.alperen@outlook.com>
2023-02-02 18:07:55 +0300

    v 0.2

--- todo.dart
+++ todo.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 void main() {
-
   procesSelection();
 }
 
@@ -9,16 +8,17 @@ final tasks = [] ;
 
 void addTodo() {
   var i = 0;
+  var number = tasks.length + 1;
+
   while (i == 0) {
-    print("Enter task: (if you don't want add task just enter. If you want return to menu just type menu) ");
+    print("Enter task: (if you want return to menu just enter)) ");
     String? task  = stdin.readLineSync();
     tasks.add(task);
     print("add sucsesfull!");
     print(tasks);
 
-    if (task == "") {
+   if (task == "") {
       i = 1;
-    }else if(task == "menu") {
       procesSelection();
     }
   }
@@ -27,7 +27,7 @@ void addTodo() {
 void deleteTodo() {
   var i = 0 ;
   while (i == 0) {
-    print("enter the delete task for the you want (if you don't want delete any task just enter. If you want return to menu just type menu)");
+    print("enter the delete task for the you want (if you want return to menu just enter)");
     String? deletedTask = stdin.readLineSync();
     tasks.remove(deletedTask);
     print("delete sucsesful!");
@@ -35,7 +35,6 @@ void deleteTodo() {
 
     if (deletedTask == "") {
       i = 1;
-    }else if(deletedTask == "menu") {
       procesSelection();
     }
   }
@@ -44,27 +43,41 @@ void deleteTodo() {
 void changeTaskSelection() {
   var i = 0;
   while (i == 0) {
-    print("Please select your task for you want to change If you want return to menu just type menu");
-    String? changeTask = stdin.readLineSync();
+    print("Please select your task for you want to change. (if you want return to menu enter task number 0 and tap enter twice.)");
+    int? taskIndex = int.parse(stdin.readLineSync()!);
 
-    print("Please enter your new task");
+    print("Please enter your new task (if you want go to menu just enter)");
     String? newTask = stdin.readLineSync();
 
-    changeTask = newTask;
-    print(tasks);
-
-    if (changeTask == "") {
+    if (taskIndex == 0) {
       i = 1;
-    }else if(changeTask == "menu") {
       procesSelection();
     }
+
+    tasks[taskIndex - 1] = newTask;
+    print(tasks);
+
+    
+  }
+}
+
+void completeTask() {
+  var i = 0;
+  while (i == 0) {
+    print("enter the complete task number");
+    int? compTask = int.parse(stdin.readLineSync()!);
+
+    tasks[compTask - 1] = tasks[compTask - 1] + " TAMAMLANDI" ;
+    print(tasks);
+    procesSelection();
   }
 }
 
 void procesSelection() {
-  print("1 - task ekle");
-  print("2 - task sil");
-  print("3 - task degistir");
+  print("1 - Task ekle");
+  print("2 - Task sil");
+  print("3 - Task degistir");
+  print("4 - Tamamlananlari isaretle");
 
   print("Yapmak istediginiz islemin numarasini girin");
   String? islemNumarasi = stdin.readLineSync();
@@ -75,5 +88,7 @@ void procesSelection() {
     deleteTodo();
   }else if(islemNumarasi == "3") {
     changeTaskSelection();
+  }else if(islemNumarasi == "4");{
+      completeTask();
   }
 }
